657a147e8f468d173b1b041f8894e1f90a257365
joakim pedersen <joakimpedersen@pop-os.localdomain>
2022-03-27 11:35:52 +0200

    trying to push to see if production site on vercel updates,

--- components/Postcard.jsx
+++ components/Postcard.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 const Postcard = ({ post }) => {
   return (
-    <div className='bg-white shadow-lg rounde-lg p-0 lg:p-8 pb-12 mb-8'>
+    <div className='bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8'>
         <div className='relative overflow-hidden shadow-md pb-80 mb-6'>
             <img src={post.featuredImage.url}
             alt={post.title} 
@@ -17,7 +17,7 @@ const Postcard = ({ post }) => {
         </h1>
         <div className='block lg:flex text-center items-center justify-center mb-8 w-full'>
             <div className='flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8'>
-                <img src={post.author.photo.url} alt={post.author.name} height='50px' width='50px' className='align-middle rounded-full'/>
+                <img src={post.author.photo.url} alt={post.author.name} height='50px' width='50px' className='rounded-full'/>
                 <p className='inline align-middle text-gray-600 font-semibold ml-2 text-lg'>{post.author.name}</p>
             </div>
             <div className='font-medium text-gray-700'>
